13c94ef867325369358dab28fcd81bf9ec6f1595
T3rebutterfly <132138488+T3rebutterfly@users.noreply.github.com>
2023-10-03 09:33:04 +0800

    java hicheel (netbeans8.1)

---
+++ sem1/durs.java
@@ -0,0 +1,8 @@
+package sem1;
+
+public class durs {
+
+    public static void main(String[] args) {
+        System.out.println("*******      ***      *      *\n*     *    *     *   ***    * *\n*     *   *       * *****  *   *\n*     *   *       *   *   *     *\n*     *   *       *   *    *   *\n*     *    *     *    *     * *\n*******      ***      *      *");
+    }    
+}
---
+++ sem1/sem2a.java
@@ -0,0 +1,9 @@
+package sem1;
+import javax.swing.JOptionPane;
+public class sem2a {
+    public static void main (String args[])
+    {
+        JOptionPane.showMessageDialog(null, "Welcome\nto\nJava\nProgramming!");
+        System.exit(0);
+    }
+}
