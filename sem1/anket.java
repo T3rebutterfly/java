4c6e51960f1334620b746e99c5dd8ee995244b42
T3rebutterfly <132138488+T3rebutterfly@users.noreply.github.com>
2023-10-03 09:46:16 +0800

    java hicheel (netbeans 8.1)

---
+++ sem1/anket.java
@@ -0,0 +1,8 @@
+package sem1;
+
+public class anket {
+
+    public static void main(String[] args) {
+        System.out.println("Ovog ner: Otgonbayar Bilguun\nGender: Male\nDate: 2004.01.04\nLocation: 46-101\nPhone: 88557729\n");
+    }    
+}
